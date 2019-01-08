32c71d3105b2fd44e1e95e03059633928ea0b5ab
Volha Kulevich <fermata@fermata.instinctools.ru>
2019-01-08 16:14:09 +0300

    divided main.js, calendar.js

--- calendar.js
+++ calendar.js
@@ -1,4 +1,6 @@
-export default function createEvent() {
+export default function setEvent(unixTimeEvent, callback) {
+    let unixTimeNow = Math.floor((new Date).getTime()/1000);
+    let interval = unixTimeEvent - unixTimeNow;
 
-
+    setTimeout(callback, interval);
 }
--- main.js
+++ main.js
@@ -1,16 +1,18 @@
-//import createEvent from './calendar.js';
+import setEvent from './calendar.js';
 
 const time = document.getElementById('date'),
     message = document.getElementById('msg'),
     btn_save = document.getElementById('btn_save');
+let msg;
 
-
+function showEvent() {
+    alert(msg);
+}
 
 btn_save.addEventListener('click', function () {
-    if (time.value.length !== 0 && message.value.length !== 0) {
-        let interval = time.value - (Math.floor((new Date).getTime()/1000));
-        setTimeout(function() {alert(message.value)}, interval);
-    }
-});
 
-//createEvent();
+    var dateEvent = time.value;
+     msg = message.value;
+
+    setEvent(dateEvent, showEvent());
+});
